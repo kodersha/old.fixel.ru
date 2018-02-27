341c19c83cbc86f0c7d24ee81af7c1ad31e5857c
Alexandra Travkina <mrs.cuteshy@gmail.com>
2018-02-27 19:47:07 +0700

    Upload

--- static/js/template.js
+++ static/js/template.js
@@ -1,3 +1,17 @@
+/* Лого */
+
+function logoAnimate() {
+	var logo = document.querySelector('[data-js="logo"]');
+	console.log(logo);
+	logo.addEventListener('mouseenter', function(e) {
+		e.target.classList.add('animate');
+	});
+};
+  
+window.onload = function() {
+	logoAnimate();
+};  
+
 /* Шапка поста */
 
 $(function() {
