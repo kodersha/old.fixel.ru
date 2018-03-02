21d8e42471740d318abc8e155dd1397c0ad49676
Alexandra Travkina <mrs.cuteshy@gmail.com>
2018-03-02 17:15:41 +0700

    new version пятница,  2 марта 2018 г. 17:15:41 (+07)

--- static/js/class.js
+++ static/js/class.js
@@ -1,10 +1,10 @@
 /* Добавляем lazyload к изображениям */
 
 $('.post p img').each(function() {
-	$(this).parent().addClass('picture lazyload');
+	$(this).parent().addClass('picture');
 	$(this).wrapAll('<a href="' + $(this).attr('src') + '" data-lightbox="image" class="image" />');
-	$(this).attr({'data-src': $(this).attr('src'), 'data-sizes': 'auto', 'class': 'lazyload'});
-	$(this).after('<noscript><img data-src="' + $(this).attr('src') + '" /></noscript>');
+	$(this).attr({'data-src': $(this).attr('src'), 'class': 'lazyload'});
+	$(this).after('<noscript><img alt="' + $(this).attr('alt') + '" src="' + $(this).attr('src') + '" /></noscript>');
 	$(this).removeAttr('src');
 });
 
