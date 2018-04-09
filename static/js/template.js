766c9428d33282aec0eff54ce29d9c4b4736683a
Alexandra Travkina <mrs.cuteshy@gmail.com>
2018-04-09 20:08:55 +0700

    new version понедельник,  9 апреля 2018 г. 20:08:54 (+07)

--- static/js/template.js
+++ static/js/template.js
@@ -1,1 +1,232 @@
-function logoAnimate(){var e=document.querySelector('[data-js="logo"]');console.log(e),e.addEventListener("mouseenter",function(e){e.target.classList.add("animate")})}var menuTop=document.getElementById("menu"),showMenu=document.getElementById("showMenu"),body=document.body;showMenu.onclick=function(){classie.toggle(this,"active"),classie.toggle(menuTop,"menu-open")},window.onload=function(){logoAnimate()},$(function(){$(".cover img").primaryColor(function(e){$(".cover .frame").css("background-color","rgb("+e+")")})}),lightbox.option({resizeDuration:0,fadeDuration:300,imageFadeDuration:300,wrapAround:!0,disableScrolling:!0,fitImagesInViewport:!0,showImageNumberLabel:!1}),$(".post .fotorama").fotorama({width:"100%",maxwidth:"100%",ratio:16/9,allowfullscreen:"native",fit:"cover"}),$(".post .youtube").each(function(){$(document).delegate("#"+this.id,"click",function(){var e="https://www.youtube.com/embed/"+this.id+"?autoplay=1&autohide=1";$(this).data("params")&&(e+="&"+$(this).data("params"));var t=$("<iframe/>",{frameborder:"0",src:e});$(this).html(t).addClass("playing"),$(this).fitVids()})}),$("[full], .full").addClass("space-minus-h-micro-xs space-minus-h-mili-md space-minus-h-base-lg space-out-h-zero-xl"),$("[color] .highlight pre").addClass("space-out-h-micro-xs space-out-h-mili-md space-out-h-base-lg"),$(".search-button").click(function(){$(".search").toggleClass("open")}),SimpleJekyllSearch({searchInput:document.getElementById("search-input"),resultsContainer:document.getElementById("results-container"),json:base_url+"/search.json",searchResultTemplate:'<li><a href="{url}" title="{title}">{title}</a></li>',noResultsText:'<li class="no-results">\u0423\u0432\u044b, \u043d\u0438\u0447\u0435\u0433\u043e \u043d\u0435 \u043d\u0430\u0439\u0434\u0435\u043d\u043e.</li>',limit:10,fuzzy:!1}),$(document).ready(function(){$(".sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close").on("click touchstart",function(e){$(".sliding-panel-content,.sliding-panel-fade-screen").toggleClass("is-visible"),e.preventDefault()})}),function(e,t,o){(t[o]=t[o]||[]).push(function(){try{t.yaCounter40455205=new Ya.Metrika({id:40455205,clickmap:!0,trackLinks:!0,accurateTrackBounce:!0,webvisor:!0})}catch(e){}});var i=e.getElementsByTagName("script")[0],n=e.createElement("script"),a=function(){i.parentNode.insertBefore(n,i)};n.type="text/javascript",n.async=!0,n.src="https://mc.yandex.ru/metrika/watch.js","[object Opera]"==t.opera?e.addEventListener("DOMContentLoaded",a,!1):a()}(document,window,"yandex_metrika_callbacks"),tocbot.init({tocSelector:".js-toc",contentSelector:".js-toc-content",headingSelector:"h2, h3"}),/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)||(!function(e,t){"use strict";t.fn.stickyFooter=function(){function o(){n="static"!==a.css("position")?a.outerHeight(!0):0,i=e.innerHeight?e.innerHeight:t(e).height(),t("body").outerHeight(!0)+n<i?a.css({position:"fixed",width:"100%",bottom:0}):a.css({position:"static",width:"auto"})}var i=0,n=0,a=this;return o(),t(e).bind("resize.stickyFooter",function(){o()}),this}}(window,jQuery),$("footer").stickyFooter()),jQuery.extend(jQuery.fn,{toplinkwidth:function(){var e=jQuery("").outerWidth(),t=jQuery(this).children("a").outerWidth(!0);return jQuery(window).width()/2-e/2-t<0?(jQuery(this).addClass("hide"),!1):(jQuery(window).scrollTop()>=1200&&jQuery(this).addClass("show"),!0)}}),jQuery(function(e){var t=e(".scroll-to-top");e(window).scroll(function(){e(window).scrollTop()>=1200&&t.toplinkwidth()?t.fadeIn(300):t.fadeOut(300)}),t.click(function(){return e("html").animate({scrollTop:0},1e3),!1})});var wow=new WOW({boxClass:"wow",animateClass:"animated",offset:0,mobile:!0,live:!0,callback:function(){},scrollContainer:null});wow.init();
+/* Меню */
+
+var menuTop = document.getElementById( 'menu' ),
+	showMenu = document.getElementById( 'showMenu' ),
+	body = document.body;
+
+showMenu.onclick = function() {
+	classie.toggle( this, 'active' );
+	classie.toggle( menuTop, 'menu-open' );
+};
+
+/* Лого */
+
+function logoAnimate() {
+	var logo = document.querySelector('[data-js="logo"]');
+	console.log(logo);
+	logo.addEventListener('mouseenter', function(e) {
+		e.target.classList.add('animate');
+	});
+};
+  
+window.onload = function() {
+	logoAnimate();
+};
+
+/* Шапка поста */
+
+$(function() {
+	$('.cover img').primaryColor(function(color) {
+		$('.cover .frame').css('background-color', 'rgb('+color+')');
+	});
+});
+
+/* Добавляем lightbox к изображениям */
+
+lightbox.option({
+	'resizeDuration': 0,
+	'fadeDuration': 300,
+	'imageFadeDuration': 300,
+	'wrapAround': true,
+	'disableScrolling': true,
+	'fitImagesInViewport': true,
+	'showImageNumberLabel': false
+});
+
+/* Настраиваем fotorama */
+
+$('.post .fotorama').fotorama({
+	width: '100%',
+	maxwidth: '100%',
+	ratio: 16/9,
+	allowfullscreen: 'native',
+	fit: 'cover'
+});
+
+/* Подменяем youtube плеер картинкой */
+
+$(".post .youtube").each(function() {
+	$(document).delegate('#'+this.id, 'click', function() {
+		var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+		if ($(this).data('params')) iframe_url+='&'+$(this).data('params');
+		var iframe = $('<iframe/>', {'frameborder': '0', 'src': iframe_url });
+		$(this).html(iframe).addClass('playing');
+		$(this).fitVids();
+	});
+});
+
+/* Добавляем классы */
+
+$( '[full], .full' ).addClass('space-minus-h-micro-xs space-minus-h-mili-md space-minus-h-base-lg space-out-h-zero-xl');
+$( '[color] .highlight pre' ).addClass('space-out-h-micro-xs space-out-h-mili-md space-out-h-base-lg');
+
+/* Поиск */
+
+$('.search-button').click(function(){
+	$(".search").toggleClass('open');
+});
+
+SimpleJekyllSearch({
+	searchInput: document.getElementById('search-input'),
+	resultsContainer: document.getElementById('results-container'),
+	json: base_url + '/search.json',
+	searchResultTemplate: '<li><a href="{url}" title="{title}">{title}</a></li>',
+	noResultsText: '<li class="no-results">Увы, ничего не найдено.</li>',
+	limit: 10,
+	fuzzy: false
+});
+$(document).ready(function() {
+	$('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart', function(e) {
+		$('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+		e.preventDefault();
+	});
+});
+
+/* Yandex */
+
+(function (d, w, c) {
+	(w[c] = w[c] || []).push(function() {
+		try {
+			w.yaCounter40455205 = new Ya.Metrika({
+				id:40455205,
+				clickmap:true,
+				trackLinks:true,
+				accurateTrackBounce:true,
+				webvisor:true
+			});
+		} catch(e) { }
+	});
+
+	var n = d.getElementsByTagName("script")[0],
+		s = d.createElement("script"),
+		f = function () { n.parentNode.insertBefore(s, n); };
+	s.type = "text/javascript";
+	s.async = true;
+	s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+	if (w.opera == "[object Opera]") {
+		d.addEventListener("DOMContentLoaded", f, false);
+	} else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+
+/* Выводим содержаение поста */
+
+tocbot.init({
+	tocSelector: '.js-toc',
+	contentSelector: '.js-toc-content',
+	headingSelector: 'h2, h3',
+});
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+
+} else {
+	/* Прилепляем футер */
+
+	;(function(window, $, undefined){
+		'use strict';
+		/**
+		 * @return {Object}
+		 */
+		$.fn.stickyFooter = function()
+		{
+			var windowHeight = 0,
+				offset = 0,
+				$div = this;
+			position();
+			$(window).bind('resize.stickyFooter', function(){
+				position();
+			});
+			/**
+			 * @return {void}
+			 */
+			function position(){
+				if( $div.css('position') !== 'static' )
+				{
+					offset = $div.outerHeight(true);
+				}else{
+					offset = 0;
+				}
+				windowHeight = window.innerHeight
+					? window.innerHeight
+					: $(window).height();
+				if( $('body').outerHeight(true) + offset < windowHeight ){
+					$div.css({
+						position	: 'fixed',
+						width		: '100%',
+						bottom		: 0,
+					});
+				}else{
+					$div.css({
+						position	: 'static',
+						width		: 'auto'
+					});
+				}
+			}
+			return this;
+		};
+	}(window, jQuery));
+
+	$('footer').stickyFooter();
+}
+
+/* Кнопка вверх */
+
+jQuery.extend(jQuery.fn, {
+	toplinkwidth: function(){
+		var totalContentWidth = jQuery('').outerWidth();
+		var totalTopLinkWidth = jQuery(this).children('a').outerWidth(true);
+		var h = jQuery(window).width()/2-totalContentWidth/2-totalTopLinkWidth;
+		if(h<0){
+			jQuery(this).addClass('hide');
+			return false;
+		} else {
+			if(jQuery(window).scrollTop() >= 1200){
+				jQuery(this).addClass('show');
+			}
+			return true;
+		}
+	}
+});
+
+jQuery(function($){
+	var topLink = $('.scroll-to-top');
+	$(window).scroll(function() {
+		if($(window).scrollTop() >= 1200 && topLink.toplinkwidth()) {
+			topLink.fadeIn(300);
+		} else {
+			topLink.fadeOut(300);
+		}
+	});
+	topLink.click(function(e) {
+		$("html").animate({scrollTop: 0}, 1000);
+		return false;
+	});
+});
+
+/* Анимации */
+
+var wow = new WOW(
+	{
+		boxClass:     'wow',      // animated element css class (default is wow)
+		animateClass: 'animated', // animation css class (default is animated)
+		offset:       0,          // distance to the element when triggering the animation (default is 0)
+		mobile:       true,       // trigger animations on mobile devices (default is true)
+		live:         true,       // act on asynchronously loaded content (default is true)
+		callback:     function(box) {
+		// the callback is fired every time an animation is started
+		// the argument that is passed in is the DOM node being animated
+		},
+		scrollContainer: null // optional scroll container selector, otherwise use window
+	}
+);
+wow.init();
