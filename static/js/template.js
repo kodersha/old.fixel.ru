b53635c186772fc929a4133cce9de1a163bc9962
Alexandra Travkina <mrs.cuteshy@gmail.com>
2018-09-18 22:56:28 +0700

    new version вторник, 18 сентября 2018 г. 22:56:28 (+07)

--- static/js/template.js
+++ static/js/template.js
@@ -93,12 +93,13 @@ $(document).ready(function() {
 (function (d, w, c) {
 	(w[c] = w[c] || []).push(function() {
 		try {
-			w.yaCounter40455205 = new Ya.Metrika({
-				id:40455205,
+			w.yaCounter50392522 = new Ya.Metrika2({
+				id:50392522,
 				clickmap:true,
 				trackLinks:true,
 				accurateTrackBounce:true,
-				webvisor:true
+				webvisor:true,
+				trackHash:true
 			});
 		} catch(e) { }
 	});
@@ -108,12 +109,12 @@ $(document).ready(function() {
 		f = function () { n.parentNode.insertBefore(s, n); };
 	s.type = "text/javascript";
 	s.async = true;
-	s.src = "https://mc.yandex.ru/metrika/watch.js";
+	s.src = "https://mc.yandex.ru/metrika/tag.js";
 
 	if (w.opera == "[object Opera]") {
 		d.addEventListener("DOMContentLoaded", f, false);
 	} else { f(); }
-})(document, window, "yandex_metrika_callbacks");
+})(document, window, "yandex_metrika_callbacks2");
 
 /* Выводим содержаение поста */
 
