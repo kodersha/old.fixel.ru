1d83661d529394b87d9e90ab3220aec957e3bbd0
Alexandra Travkina <mrs.cuteshy@gmail.com>
2018-09-29 01:53:58 +0700

    new version Sat Sep 29 01:53:57 STD 2018

--- static/js/ads.js
+++ static/js/ads.js
@@ -5,11 +5,10 @@
 	- https://yuukithemes.com/
 */
 
-var msg1=Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-var msg2=Math.random().toString(36).substring(2, 7);
-var e=document.getElementById("money");e.setAttribute("id", msg1);
-e.classList.add("money_" + msg2);
+var mesg1=Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+var mesg2=Math.random().toString(36).substring(2, 7);
+var e=document.getElementById("money");e.setAttribute("id", mesg1);
+e.classList.add("money_" + mesg2);
 if (!window.showAds) {
-	document.getElementById('message').style.visibility = "visible";
 	document.getElementById('message').style.display = "table";
 }
